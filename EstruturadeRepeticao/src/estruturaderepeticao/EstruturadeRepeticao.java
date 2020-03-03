520d367a8195ccb1f804d2d370a50c066610b24d
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-03 15:28:02 -0300

    Atualizando

---
+++ EstruturadeRepeticao/src/estruturaderepeticao/EstruturadeRepeticao.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturaderepeticao;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guilb
+ */
+public class EstruturadeRepeticao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("quantas cambalhotas você deseja");
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        int a = teclado.nextInt();
+        int b = 0;
+        
+        while (b < a)
+        {
+            b++;
+            if (b == 5) continue;
+            System.out.println("while " + b);
+            
+        }
+
+    }
+    
+}
